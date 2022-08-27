a9bc2a0c08fb8158e0790ea392cc0e0cc38daa07
dyshendre <103635214+dyshendre@users.noreply.github.com>
2022-08-27 23:19:39 +0530

    added

---
+++ 0Final_2222/0Final_2/components/sidebar.js
@@ -0,0 +1,45 @@
+
+function sidebar(){
+    console.lpg("yes");
+    return `  <div id="shop_sidebar1">
+    <h2 id="shop_price">Price</h2>
+    <p id="shop_price_1"><input class="checkbox" type="checkbox">₹10.00 - ₹49.00</p>
+    <p id="shop_price_2"><input class="checkbox" type="checkbox">₹50.00 - ₹99.00</p>
+    <p id="shop_price_3"><input class="checkbox" type="checkbox">₹100.00 - ₹199.00</p>
+    <p id="shop_price_4"><input class="checkbox" type="checkbox">₹200.00 +</p>
+</div>
+<div id="shop_sidebar2">
+    
+        <h2 id="shop_brand">Brand</h2>
+       
+        <input type="text" id="brand_search" placeholder="Search brand products">
+        <p id="shop_brand_1"><input class="checkbox" type="checkbox">26 carat</p>
+        <p id="shop_brand_2"><input class="checkbox" type="checkbox">DANIEL CHRISTIAN</p>
+        <p id="shop_brand_3"><input class="checkbox" type="checkbox">Finachi</p>
+        <p id="shop_brand_4"><input class="checkbox" type="checkbox">hepo</p>
+        <p id="shop_brand_5"><input class="checkbox" type="checkbox">jacksapprow</p>
+        <p id="shop_brand_6"><input class="checkbox" type="checkbox">MITALI</p>
+        <p id="shop_brand_7"><input class="checkbox" type="checkbox">philips</p>
+        <p id="shop_brand_8"><input class="checkbox" type="checkbox">piirona</p>
+   
+  
+</div>
+<div id="shop_sidebar3" >
+    <h2 id="shop_category">Category</h2>
+    <p id="shop_category_1"><input class="checkbox" type="checkbox">Uncategorized</p>
+    <!-- <p id="shop_category_2"><input class="checkbox" type="checkbox"></p> -->
+    <p id="shop_category_3"><input class="checkbox" type="checkbox">Bathroom Accessories (9)</p>
+    <p id="shop_category_4"><input class="checkbox" type="checkbox">Electronics (151)</p>
+    <p id="shop_category_5"><input class="checkbox" type="checkbox">Clothing</p>
+    <p id="shop_category_6"><input class="checkbox" type="checkbox">Watches10</p>
+    <p id="shop_category_7"><input class="checkbox" type="checkbox">Women Accessories</p>
+    <p id="shop_category_8"><input class="checkbox" type="checkbox">Grocery (8)</p>
+    <p id="shop_category_9"><input class="checkbox" type="checkbox">Health & Personal Care (176)</p>
+    <p id="shop_category_10"><input class="checkbox" type="checkbox">Household Supplies (4)</p>
+    <p id="shop_category_11"><input class="checkbox" type="checkbox">Industrial Supplies (6)</p>
+    <p id="shop_category_12"><input class="checkbox" type="checkbox">Kitchen Ware205</p>
+
+</div>`;
+}
+
+export default sidebar;
---
+++ 0Final_2222/0Final_2/scripts/filter.js
@@ -0,0 +1,49 @@
+
+
+import append from "./append_data.js";
+    function filterData(data2){
+        // let data2=JSON.parse(localStorage.getItem("dataFilter"));
+        // console.log("data2:",data2);
+        
+        let filter1 = document.getElementById("shop_price_1")
+        filter1.addEventListener("click", function () {
+          console.log("yes");
+          let f1 = data2.filter(function (el) {
+            return (el.price < "1000");
+          });
+          console.log(f1);
+          append(f1);
+        })
+      
+        let filter2 = document.getElementById("shop_price_2")
+        filter2.addEventListener("click", function () {
+          console.log("yes");
+          let f1 = data2.filter(function (el) {
+            return ("1000" < el.price && el.price < "2000");
+          });
+          console.log(f1);
+          append(f1);
+        })
+      
+        let filter3 = document.getElementById("shop_price_3")
+        filter3.addEventListener("click", function () {
+          console.log("yes");
+          let f1 = data2.filter(function (el) {
+            return ("2000" < el.price && el.price < "3000");
+          });
+          console.log(f1);
+          append(f1);
+        })
+      
+        let filter4 = document.getElementById("shop_price_4")
+        filter4.addEventListener("click", function () {
+          console.log("yes");
+          let f1 = data2.filter(function (el) {
+            return (el.price > "3000");
+          });
+          console.log(f1);
+          append(f1);
+        })
+        }
+
+        export {filterData};
